f7b09455ef4c8385f7a516b20b1bb8d8f863caf6
SabbirHasan011 <sabbirhfahim@gmail.com>
2026-02-04 16:28:00 +0600

    new folder added

---
+++ CPPs/loop.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define fios(); ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    fios();
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        cout<<"sabbir is really smart"<<endl;
+    }
+} 
