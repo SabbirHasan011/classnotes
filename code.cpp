e339fb463529b6490219e76e49965ae3da4cfdf1
SabbirHasan011 <sabbirhfahim@gmail.com>
2026-02-04 16:25:40 +0600

    cpp file added

---
+++ code.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+#define fios(); ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    fios();
+    cout<<"this is a git repo"<<endl;
+} 
